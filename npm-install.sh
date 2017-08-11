84089f944e106226b868b9a034491389d707bb25
Simon Aunér <simonauner@gmail.com>
2017-08-11 07:53:35 +0200

    add setting strict ssl to false for npm

--- npm-install.sh
+++ npm-install.sh
@@ -1,4 +1,7 @@
 
+# set strict ssl false to get around corporate proxy
+npm config set strict-ssl false
+
 # if permission problems arise: https://docs.npmjs.com/getting-started/fixing-npm-permissions
 
 # global npm tools
